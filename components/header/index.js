cb348681da20f7af4d8f1267c9ebdc28c003aed7
Osvaldo <osvarz.new@gmail.com>
2021-07-04 11:59:43 -0500

    🎨 improved styles in header component

--- components/header/index.js
+++ components/header/index.js
@@ -2,7 +2,7 @@ import Navbar from 'components/navbar'
 
 export default function Header() {
   return (
-    <section className="relative z-30 bg-transparent text-white font-manrope bg-gradient-to-r from-black-light via-black-light to-transparent">
+    <section className="bg-black-light relative z-30 bg-transparent text-white font-manrope bg-gradient-to-r from-black-light via-black-light to-transparent">
       <div className="flex justify-between w-10/12 mx-auto py-9 border-b border-white border-opacity-20">
         <div>
           <img src="/img/shared/desktop/logo.svg" alt="logo" />
