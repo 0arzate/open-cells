c77c9aa4fd7611ad30f5fdb169b751334fd2cf90
Osvaldo <osvarz.new@gmail.com>
2021-07-04 11:16:09 -0500

    🎨 home description component created

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Header from 'components/header'
 import HomeHero from 'components/home-hero'
 import HomeShop from 'components/home-shop'
 import HomeProduct from 'components/home-product'
+import HomeDescription from 'components/home-description'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <HomeHero />
         <HomeShop />
         <HomeProduct />
+        <HomeDescription />
       </main>
     </>
   )
