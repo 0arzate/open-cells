ccff94ffeea97aee9fe5f4d2f66966faebc3e159
Osvaldo <osvarz.new@gmail.com>
2021-07-04 18:01:07 -0500

    ✨ product page connected with backend

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,24 +2,35 @@ import { useState, useEffect } from 'react'
 import { useRouter } from 'next/dist/client/router'
 
 import Layout from 'components/layout'
+import ProductCard from 'components/product-card'
 
 export default function Product() {
-  const [product, setProduct] = useState({})
-  const Router = useRouter()
+  const [product, setProduct] = useState(false)
+  const router = useRouter()
 
-  const getProduct = () => {
-    console.log(Router)
-    console.log(product)
+  const getProduct = async () => {
+    const url =
+      window.location.origin === 'http://localhost:3000'
+        ? 'http://localhost:3000/api'
+        : 'https://audiophile-rho.vercel.app/api'
+
+    const request = await fetch(url + `/all`)
+    const response = await request.json()
+    const product = await response.filter(
+      (product) => product.slug === router.query.slug
+    )
+    setProduct(product[0])
   }
 
   useEffect(() => {
     getProduct()
-    setProduct({})
-  }, [])
+  }, [router])
 
   return (
     <Layout>
-      <p></p>
+      <section className="w-10/12 mx-auto">
+        {product ? <ProductCard product={product} reverse={true} /> : null}
+      </section>
     </Layout>
   )
 }
