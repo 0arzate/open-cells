a71464e301dfa032a702280c866f6a657a40f034
Osvaldo <osvarz.new@gmail.com>
2021-07-04 13:36:33 -0500

    ✨ product list connected to the API

--- components/product-list/index.js
+++ components/product-list/index.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/dist/client/router'
+
+import ProductCard from 'components/product-card'
+
 export default function ProductList() {
+  const [product, setProduct] = useState([])
+  const Router = useRouter()
+
+  const getData = async () => {
+    const product = Router.asPath
+    const url =
+      window.location.origin === 'http://localhost:3000'
+        ? 'http://localhost:3000/api'
+        : 'https://planets-fact.vercel.app/api/'
+    const request = await fetch(url + product)
+    const response = await request.json()
+    setProduct(response)
+  }
+
+  const renderProduct = () =>
+    product.map((product) => <ProductCard key={product.id} product={product} />)
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <section className="py-64">
-      <div></div>
+      <div>{renderProduct()}</div>
     </section>
   )
 }
