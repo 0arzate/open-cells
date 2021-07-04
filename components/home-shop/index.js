8d1d642a2229228c2cd78dd73e14e806f5a513f4
Osvaldo <osvarz.new@gmail.com>
2021-07-04 14:36:11 -0500

    🎨 improved styles

--- components/home-shop/index.js
+++ components/home-shop/index.js
@@ -4,7 +4,7 @@ import IconButton from 'components/icon-button'
 export default function HomeShop() {
   return (
     <section className="bg-white pt-48 pb-64">
-      <div className="flex w-10/12 mx-auto text-black font-bold text-2md leading-lg">
+      <div className="flex justify-center w-10/12 mx-auto text-black font-bold text-2md leading-lg">
         <div className="flex flex-col justify-between mr-8 text-center bg-gradient-to-b via-gray-own from-white to-gray-own pb-7">
           <img
             className="w-9/12 mx-auto"
