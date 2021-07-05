7b16ff49a83a9c0d29347cad15603bcd7fc9d012
Osvaldo <osvarz.new@gmail.com>
2021-07-04 20:21:17 -0500

    🎨 improved product description component

--- components/product-description/index.js
+++ components/product-description/index.js
@@ -1,25 +1,28 @@
 export default function ProductDescription({ product }) {
   return (
-    <section>
-      <div>
+    <section className="flex justify-between">
+      <div className="w-7/12">
         <h2 className="uppercase mb-8 font-bold text-2xl leading-3xl">
           FEATURES
         </h2>
-        <p className="text-black text-opacity-50 text-2sm leading-lg">
+        <p className="mb-8 text-black text-opacity-50 text-2sm leading-lg">
           {product.features}
         </p>
+        <p className="text-black text-opacity-50 text-2sm leading-lg">
+          {product.description}
+        </p>
       </div>
-      <div>
+      <div className="w-4/12">
         <h2 className="uppercase mb-8 font-bold text-2xl leading-3xl">
           in the box
         </h2>
         <ul>
           {product.includes.map((include) => (
             <li
-              className="text-black text-opacity-50 text-2sm leading-lg"
+              className="text-black mb-2 text-opacity-50 text-2sm leading-lg"
               key={include.item}
             >
-              <span className="text-orange-own">{`${include.quantity}x`}</span>
+              <span className="mr-6 text-orange-own">{`${include.quantity}x`}</span>
               {include.item}
             </li>
           ))}
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,8 +2,10 @@ import { useState, useEffect } from 'react'
 import { useRouter } from 'next/dist/client/router'
 import { URL_API } from 'services/path'
 
+import IconButton from 'components/icon-button'
 import Layout from 'components/layout'
 import ProductCard from 'components/product-card'
+import ProductDescription from 'components/product-description'
 
 export default function Product() {
   const [product, setProduct] = useState(false)
@@ -24,8 +26,12 @@ export default function Product() {
 
   return (
     <Layout>
-      <section className="w-10/12 mx-auto">
+      <section className="w-10/12 mx-auto pt-18 pb-40">
+        <div className="mb-14">
+          <IconButton name="Go back" iconName="" />
+        </div>
         {product ? <ProductCard product={product} reverse={true} /> : null}
+        <ProductDescription product={product} />
       </section>
     </Layout>
   )
