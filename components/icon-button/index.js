15a14ab5a4914495397b090a844ca8efdde16c8d
Osvaldo <osvarz.new@gmail.com>
2021-07-05 05:48:28 -0500

    🎨 improved icon button component

--- components/icon-button/index.js
+++ components/icon-button/index.js
@@ -3,11 +3,13 @@ export default function IconButton({
   iconName = 'fas fa-chevron-right',
   margin,
   iconSize = '12px',
+  onClick,
 }) {
   return (
     <button
       className="inline-flex items-center uppercase text-black hover:text-orange-own text-opacity-50 hover:text-opacity-100"
       style={{ margin: margin }}
+      onClick={onClick}
     >
       {name}
       <i
