4e3ee02ccd3701eb12c6e7a349d7f87eac6d112b
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-04-27 08:07:24 -0500

    💄 cart added

---
+++ src/components/Cart.js
@@ -0,0 +1,25 @@
+import ProductCart from '@components/ProductCart'
+import Button from '@components/Button'
+
+export default function Cart() {
+  const currentProducts = 3
+
+  return (
+    <div className="p-8 bg-white w-[378px] absolute top-[129px] right-[165px] rounded-lg">
+      <div className="flex items-center justify-between text-black mb-8">
+        <p className="text-lg font-bold">{`CART ( ${currentProducts} )`}</p>
+        <p className="text-orange-own text-sm cursor-pointer hover:underline">
+          Remove all
+        </p>
+      </div>
+      <div>
+        <ProductCart />
+      </div>
+      <div className="flex items-center justify-between my-8">
+        <p className="text-sm text-opacity-50">TOTAL</p>
+        <p className="font-bold text-lg">{`$ 5,396`}</p>
+      </div>
+      <Button solid={true} name="checkout" full />
+    </div>
+  )
+}
