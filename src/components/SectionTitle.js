263ff909999076cfe41c56d3b04aa3e9d3d8d4ce
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2025-10-07 22:19:14 -0600

    feat(clean-project): 🔥 clean react project to build with cells

--- src/components/SectionTitle.js
+++
@@ -1,9 +0,0 @@
-export default function SectionTitle({ title = 'titulo' }) {
-  return (
-    <div className="bg-black py-24">
-      <div className="w-10/12 2xl:w-8/12 mx-auto uppercase font-bold text-4xl text-center text-white">
-        <h2>{title}</h2>
-      </div>
-    </div>
-  )
-}
--- src/utils/currencyUtils.js
+++
@@ -1,8 +0,0 @@
-export const USDFormat = (money) => {
-  const currencyFormat = Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  })
-
-  return currencyFormat.format(money)
-}
