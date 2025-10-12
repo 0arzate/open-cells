9f48d70666180b8681298ec39216428699883062
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2025-10-12 07:38:50 -0600

    feat(config): 🔥 remove useless constants

--- app/utils/constants/index.js
+++ app/utils/constants/index.js
@@ -5,17 +5,7 @@ export const LANGUAGES = Object.freeze({
   'en-US': 'es-MX'
 })
 
-export const LANGUAGES_LABELS = Object.freeze({
-  'es-MX': 'languages.es-mx',
-  'en-US': 'languages.en-us'
-})
-
 export const THEMES = Object.freeze({
   LIGHT_MODE: 'light_mode',
   DARK_MODE: 'dark_mode'
-})
-
-export const THEME_LABELS = Object.freeze({
-  dark_mode: '🌙',
-  light_mode: '☀️'
 })
